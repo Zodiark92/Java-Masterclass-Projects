e902a518da234ec0098fb5508af5a4ce6c095c67
Zodiark92 <91218477+Zodiark92@users.noreply.github.com>
2025-01-25 17:15:41 +0100

    Packages

---
+++ untitled/src/company/abc/Item.java
@@ -0,0 +1,17 @@
+package company.abc;
+
+public class Item {
+
+    private String type;
+
+    public Item(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
---
+++ untitled/src/dev/lpa/Main.java
@@ -0,0 +1,12 @@
+package dev.lpa;
+
+import company.abc.Item; //import of the class in the package
+
+public class Main {
+
+    public static void main(String[] args) {
+        Item burger = new Item("Burger");
+        System.out.println(burger);
+    }
+    
+}
