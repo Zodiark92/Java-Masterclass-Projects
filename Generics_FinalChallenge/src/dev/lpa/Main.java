9b3d89d6dfce6cc7d7158ad633ed232f20bc30ba
Zodiark92 <91218477+Zodiark92@users.noreply.github.com>
2025-03-21 18:15:15 +0100

    Generics challenge - other comparators

--- Generics_FinalChallenge/src/dev/lpa/Main.java
+++ Generics_FinalChallenge/src/dev/lpa/Main.java
@@ -2,6 +2,8 @@ package dev.lpa;
 
 import dev.lpa.model.LPAStudent;
 import dev.lpa.model.Student;
+import dev.lpa.util.QueryItem;
+import dev.lpa.util.QueryList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,7 +21,7 @@ public class Main {
             students.add(new LPAStudent());
         }
 
-        List<Student> queryStudents = new ArrayList<>();
+        QueryList<LPAStudent> queryStudents = new QueryList<>();
         for(var student : students) {
             if(student.matchFieldValue("PERCENT_COMPLETE", "50.0")) {
                 queryStudents.add(student);
@@ -32,23 +34,51 @@ public class Main {
 
         queryStudents.sort(Comparator.naturalOrder());
 
+        System.out.println("*".repeat(30));
+        System.out.println("Student Order ID: ");
+        queryStudents.sort(new IDComparator());
+        System.out.println("QueryStudents: \n" + queryStudents);
 
+        System.out.println("*".repeat(30));
+        System.out.println("Student Order Percent Complete:: ");
+        queryStudents.sort(new PercentCompleteComparator());
+        System.out.println("QueryStudents: \n" + queryStudents);
 
-        StudentComparator comparator = new  StudentComparator();
-        LPAStudent[] lpaStudents = (LPAStudent[]) Arrays.copyOf(queryStudents.toArray(), queryStudents.size());
-
-        Arrays.sort(lpaStudents, comparator);
-        System.out.println("QueryStudents: \n" + lpaStudents);
-
+        System.out.println("*".repeat(30));
+        System.out.println("Student Order Year: ");
+        queryStudents.sort(new YearComparator());
+        System.out.println("QueryStudents: \n" + queryStudents);
 
     }
 
 }
 
-class StudentComparator implements Comparator<LPAStudent> {
+class IDComparator implements Comparator<LPAStudent> {
 
     @Override
     public int compare(LPAStudent o1, LPAStudent o2) {
         return o1.compareTo(o2);
     }
 }
+
+class PercentCompleteComparator implements Comparator<LPAStudent> {
+
+    //with percent complete
+    @Override
+    public int compare(LPAStudent o1, LPAStudent o2) {
+        return o1.compareByPercentComplete(o2);
+    }
+
+}
+
+class YearComparator implements Comparator<LPAStudent> {
+
+    //with year order
+    @Override
+    public int compare(LPAStudent o1, LPAStudent o2) {
+        return o1.compareByYear(o2);
+    }
+}
+
+
+
